eaa9bef7ac8881bc7bd53fba76caf9b6131c11ec
asmz <land.front.high.field@gmail.com>
2025-02-25 09:51:47 +0900

    feat: [Android] ページング処理実装

--- composeApp/src/androidMain/kotlin/beer/asmz/portfolio/kmp/ui/common/post/PostList.kt
+++ composeApp/src/androidMain/kotlin/beer/asmz/portfolio/kmp/ui/common/post/PostList.kt
@@ -1,12 +1,27 @@
 package beer.asmz.portfolio.kmp.ui.common.post
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import beer.asmz.portfolio.kmp.constant.AccentColor
 import beer.asmz.portfolio.kmp.model.Post
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun PostList(
     viewModel: PostListViewModel,
@@ -14,10 +29,44 @@ fun PostList(
 ) {
     val posts by viewModel.posts.collectAsStateWithLifecycle()
     val isLoading by viewModel.isLoading.collectAsStateWithLifecycle()
+    val isRefreshing by viewModel.isRefreshing.collectAsStateWithLifecycle()
 
-    LazyColumn {
-        items(posts) { post ->
-            PostItem(post = post, onPress = onPress)
+    val pullRefreshState = rememberPullRefreshState(
+        refreshing = isRefreshing,
+        onRefresh = { viewModel.refresh() }
+    )
+    Box(modifier = Modifier.pullRefresh(pullRefreshState)) {
+        LazyColumn {
+            items(posts, key = { it.idString }) { post ->
+                PostItem(post = post, onPress = onPress)
+            }
+            item {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp, bottom = 32.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    if (isLoading && !isRefreshing) {
+                        CircularProgressIndicator(
+                            modifier = Modifier.width(32.dp),
+                            color = AccentColor,
+                        )
+                    }
+                }
+            }
+            item {
+                LaunchedEffect(true) {
+                    viewModel.fetchPosts()
+                }
+            }
         }
+        PullRefreshIndicator(
+            refreshing = isRefreshing,
+            state = pullRefreshState,
+            modifier = Modifier
+                .align(Alignment.TopCenter),
+            contentColor = AccentColor,
+        )
     }
 }
--- composeApp/src/androidMain/kotlin/beer/asmz/portfolio/kmp/ui/common/post/PostListViewModel.kt
+++ composeApp/src/androidMain/kotlin/beer/asmz/portfolio/kmp/ui/common/post/PostListViewModel.kt
@@ -12,9 +12,14 @@ import kotlinx.coroutines.launch
 
 class PostListViewModel(private val tag: PostTag) : ViewModel() {
     private val repository = ThumblrRepository()
+    private var offset = 0
+    private var hasNext = true
 
     private val _isLoading = MutableStateFlow(false)
     val isLoading: StateFlow<Boolean> get() = _isLoading
+
+    private val _isRefreshing = MutableStateFlow(false)
+    val isRefreshing: StateFlow<Boolean> get() = _isRefreshing
 
     private val _posts = MutableStateFlow<List<Post>>(emptyList())
     val posts: StateFlow<List<Post>> get() = _posts
@@ -23,23 +28,32 @@ class PostListViewModel(private val tag: PostTag) : ViewModel() {
         const val LIMIT = 20
     }
 
-    init {
-        fetchPosts()
-    }
-
-    private fun fetchPosts() {
+    fun fetchPosts() {
+        if (!hasNext || _isLoading.value) return
         _isLoading.update { true }
 
         viewModelScope.launch {
             val params = mapOf(
-                "offset" to "0",
+                "offset" to "$offset",
                 "limit" to "$LIMIT",
                 "tag" to tag.name.lowercase()
             )
 
             val response = repository.getPosts(params)
-            _posts.update { response.posts }
+            _posts.update { it + response.posts }
+            offset += LIMIT
+            hasNext = response.totalPosts > _posts.value.count()
             _isLoading.update { false }
+            _isRefreshing.update { false }
         }
+    }
+
+    fun refresh() {
+        _isRefreshing.update { true }
+        _posts.update { emptyList() }
+        offset = 0
+        hasNext = true
+
+        fetchPosts()
     }
 }
